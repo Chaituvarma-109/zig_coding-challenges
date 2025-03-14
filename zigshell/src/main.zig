93e173ec41c3c3eab77d5f6c7ff0fc3768de01cb
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2025-03-15 03:09:49 +0530

    Add shell history and started working for pipe command skeleton
    This is the initial work for command history and piping support in
    the shell. Adds history file tracking.

--- zigshell/src/main.zig
+++ zigshell/src/main.zig
@@ -1,60 +1,7 @@
 const std = @import("std");
 
 const stdout = std.io.getStdOut().writer();
-
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    while (true) {
-        try stdout.print("$ ", .{});
-
-        const stdin = std.io.getStdIn().reader();
-        var buffer: [1024]u8 = undefined;
-        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-
-        const trim_inp = std.mem.trim(u8, user_input, "\r\n");
-        var token_iter = std.mem.splitSequence(u8, trim_inp, " ");
-
-        const cmd = token_iter.first();
-        var args = token_iter.rest();
-
-        if (std.mem.eql(u8, cmd, "exit")) {
-            std.posix.exit(0);
-        } else if (std.mem.eql(u8, cmd, "cd")) {
-            const home: []const u8 = "HOME";
-            if (std.mem.eql(u8, args, "~")) {
-                args = std.posix.getenv(home) orelse "";
-            }
-            std.posix.chdir(args) catch {
-                try stdout.print("{s}: No such file or directory\n", .{args});
-            };
-        } else if (std.mem.eql(u8, cmd, "pwd")) {
-            var buff: [std.fs.MAX_PATH_BYTES]u8 = undefined;
-            const pwd = try std.process.getCwd(&buff);
-
-            try stdout.print("{s}\n", .{pwd});
-        } else if (std.mem.eql(u8, cmd, "ls")) {
-            if (args.len == 0) {
-                var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
-                var iter = dir.iterate();
-
-                while (try iter.next()) |entry| {
-                    if (entry.name.len > 0 and entry.name[0] == '.') {
-                        continue;
-                    }
-                    try stdout.print("{s} ", .{entry.name});
-                }
-                try stdout.print("\n", .{});
-            } else {
-                try runExternalCmd(alloc, cmd, args);
-            }
-        } else {
-            try runExternalCmd(alloc, cmd, args);
-        }
-    }
-}
+const hst_path: []const u8 = ".shell_history";
 
 fn runExternalCmd(alloc: std.mem.Allocator, cmd: []const u8, args: []const u8) !void {
     if (try typeBuilt(alloc, cmd)) |p| {
@@ -77,4 +24,93 @@ fn typeBuilt(alloc: std.mem.Allocator, args: []const u8) !?[]const u8 {
     }
 
     return null;
+}
+
+fn writeToFile(file: std.fs.File, cmd: []const u8, arg: []const u8) !void {
+    try file.seekFromEnd(0);
+    try file.writeAll(cmd);
+    if (arg.len != 0) {
+        try file.writeAll(" ");
+        try file.writeAll(arg);
+    }
+    try file.writeAll("\n");
+}
+
+fn executePipeCmds() !void {}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    std.posix.access(hst_path, 0) catch {
+        const file = try std.fs.cwd().createFile(hst_path, .{ .read = true });
+        defer file.close();
+    };
+
+    const file = try std.fs.cwd().openFile(hst_path, .{ .mode = .read_write });
+    defer file.close();
+
+    while (true) {
+        try stdout.print("ccshell> ", .{});
+
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        const trim_inp = std.mem.trim(u8, user_input, "\r\n");
+        var token_iter = std.mem.tokenizeSequence(u8, trim_inp, "|");
+
+        const cmds = token_iter.next().?;
+        const rest_cmds = token_iter.rest();
+
+        if (rest_cmds.len != 0) {
+            try executePipeCmds();
+        }
+
+        var cmds_iter = std.mem.tokenizeScalar(u8, cmds, ' ');
+
+        const cmd = cmds_iter.next().?;
+        var args = cmds_iter.rest();
+
+        if (std.mem.eql(u8, cmd, "exit")) {
+            try writeToFile(file, cmd, args);
+            std.posix.exit(0);
+        } else if (std.mem.eql(u8, cmd, "cd")) {
+            const home: []const u8 = "HOME";
+            if (std.mem.eql(u8, args, "~")) {
+                args = std.posix.getenv(home) orelse "";
+            }
+            try writeToFile(file, cmd, args);
+            std.posix.chdir(args) catch {
+                try stdout.print("{s}: No such file or directory\n", .{args});
+            };
+        } else if (std.mem.eql(u8, cmd, "pwd")) {
+            var buff: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+            const pwd = try std.process.getCwd(&buff);
+
+            try stdout.print("{s}\n", .{pwd});
+            try writeToFile(file, cmd, args);
+        } else if (std.mem.eql(u8, cmd, "ls")) {
+            if (args.len == 0) {
+                var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+                var iter = dir.iterate();
+                try writeToFile(file, cmd, args);
+
+                while (try iter.next()) |entry| {
+                    if (entry.name.len > 0 and entry.name[0] == '.') {
+                        continue;
+                    }
+                    try stdout.print("{s} ", .{entry.name});
+                }
+                try stdout.print("\n", .{});
+            } else {
+                try writeToFile(file, cmd, args);
+                try runExternalCmd(alloc, cmd, args);
+            }
+        } else {
+            try writeToFile(file, cmd, args);
+            try runExternalCmd(alloc, cmd, args);
+        }
+    }
 }
