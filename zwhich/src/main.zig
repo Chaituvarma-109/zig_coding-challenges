ee4ed171654363a7829a1e4bcd3e8cdcefd9b6a0
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2025-11-01 10:13:30 +0530

    modified code.

--- zwhich/src/main.zig
+++ zwhich/src/main.zig
@@ -1,27 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var gpa = std.heap.DebugAllocator(.{}).init;
-    const alloc = gpa.allocator();
-    defer _ = gpa.deinit();
+    const alloc = std.heap.page_allocator;
 
-    var paths_arr: std.ArrayList([]const u8) = .empty;
-    defer paths_arr.deinit(alloc);
+    var args = std.process.args();
+    _ = args.skip();
 
     const paths = std.posix.getenv("PATH") orelse return error.patherror;
     var path_iter = std.mem.tokenizeAny(u8, paths, ":");
-    while (path_iter.next()) |path| {
-        try paths_arr.append(alloc, path);
-    }
 
-    const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
-
-    const commands = args[1..];
-
-    for (commands) |command| {
-        for (paths_arr.items) |dir| {
-            const full_path = std.fs.path.join(alloc, &[_][]const u8{ dir, command }) catch continue;
+    while (args.next()) |command| {
+        while (path_iter.next()) |path| {
+            const full_path = std.fs.path.join(alloc, &[_][]const u8{ path, command }) catch continue;
             defer alloc.free(full_path);
 
             // you can also use std.fs.cwd().access(full_path, .{}) catch continue; and std.fs.accessAbsolute(full_path, .{}) catch continue;
