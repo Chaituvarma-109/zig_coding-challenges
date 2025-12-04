4a92bcecc0bf5aab803366725f9f80e58fdff164
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2025-12-04 17:11:29 +0530

    refactored code.

--- wc/src/main.zig
+++ wc/src/main.zig
@@ -1,74 +1,98 @@
 const std = @import("std");
 
-var lines: usize = 0;
-var words: usize = 0;
-var chars: usize = 0;
-var bytes: usize = 0;
-var in_word: bool = false;
+const Config = struct {
+    options: ?[]const u8 = null,
+    filename: ?[]const u8 = null,
+
+    fn init() !Config {
+        var args = std.process.args();
+        _ = args.skip();
+        var cliargs = Config{};
+
+        while (args.next()) |arg| {
+            if (arg.len >= 2 and std.mem.startsWith(u8, arg, "-")) {
+                cliargs.options = arg[1..arg.len];
+            } else {
+                cliargs.filename = arg;
+            }
+        }
+
+        return cliargs;
+    }
+};
+
+const Wc = struct {
+    lines: usize = 0,
+    words: usize = 0,
+    chars: usize = 0,
+    bytes: usize = 0,
+
+    fn count(config: Config) !Wc {
+        var wc = Wc{};
+        var in_word: bool = false;
+        var fd: std.posix.fd_t = std.posix.STDIN_FILENO;
+
+        if (config.filename) |f_name| {
+            fd = try std.posix.open(f_name, .{ .ACCMODE = .RDONLY }, 0);
+        }
+        defer if (config.filename != null) std.posix.close(fd);
+
+        const stat = try std.posix.fstat(fd);
+        const is_regular: bool = (stat.mode & std.posix.S.IFMT) == std.posix.S.IFREG;
+
+        if (is_regular) {
+            _ = std.os.linux.fadvise(fd, 0, 0, std.os.linux.POSIX_FADV.SEQUENTIAL);
+        }
+        var buff: [256 * 1024]u8 = undefined;
+
+        while (true) {
+            const n: usize = try std.posix.read(fd, &buff);
+            if (n == 0) break;
+
+            const data: []const u8 = buff[0..n];
+            wc.bytes += data.len;
+
+            for (data) |char| {
+                if ((char <= 0x7f) or (char >= 0xc0 and char <= 0xf7)) wc.chars += 1;
+
+                if (char == '\n') wc.lines += 1;
+
+                if (std.ascii.isWhitespace(char)) {
+                    if (in_word) in_word = false;
+                } else {
+                    if (!in_word) {
+                        in_word = true;
+                        wc.words += 1;
+                    }
+                }
+            }
+        }
+        return wc;
+    }
+};
 
 pub fn main() !void {
-    var options: u8 = undefined;
-    var file_name: ?[]const u8 = null;
+    const config: Config = try .init();
 
-    var args = std.process.args();
-    _ = args.skip();
+    const count: Wc = try .count(config);
+    const options: ?[]const u8 = config.options;
 
-    while (args.next()) |arg| {
-        if (arg.len > 1 and std.mem.startsWith(u8, arg, "-")) {
-            options = arg[1];
+    if (options) |opts| {
+        for (opts) |opt| {
+            switch (opt) {
+                'c' => std.debug.print("{d} ", .{count.bytes}),
+                'l' => std.debug.print("{d} ", .{count.lines}),
+                'w' => std.debug.print("{d} ", .{count.words}),
+                'm' => std.debug.print("{d} ", .{count.chars}),
+                else => {},
+            }
+        }
+        std.debug.print("{s}\n", .{config.filename.?});
+    } else {
+        if (config.filename) |f_name| {
+            std.debug.print("{d} {d} {d} {s}\n", .{ count.lines, count.words, count.bytes, f_name });
         } else {
-            file_name = arg;
+            std.debug.print("{d} {d} {d}\n", .{ count.lines, count.words, count.bytes });
         }
-    }
-
-    var fd: std.posix.fd_t = std.posix.STDIN_FILENO;
-    if (file_name) |f_name| {
-        fd = std.posix.open(f_name, .{ .ACCMODE = .RDONLY }, 0) catch |err| {
-            std.debug.print("Error in opening file: {any}\n", .{err});
-            return;
-        };
-    }
-    defer if (file_name != null) std.posix.close(fd);
-
-    const stat = try std.posix.fstat(fd);
-    const is_regular: bool = (stat.mode & std.posix.S.IFMT) == std.posix.S.IFREG;
-
-    if (is_regular) _ = std.os.linux.fadvise(fd, 0, 0, std.os.linux.POSIX_FADV.SEQUENTIAL);
-
-    var buff: [256 * 1024]u8 = undefined;
-
-    while (true) {
-        const n: usize = try std.posix.read(fd, &buff);
-        if (n == 0) break;
-
-        const data: []const u8 = buff[0..n];
-        bytes += data.len;
-
-        for (data) |char| {
-            if ((char <= 0x7f) or (char >= 0xc0 and char <= 0xf7)) chars += 1;
-
-            if (char == '\n') lines += 1;
-
-            if (std.ascii.isWhitespace(char)) {
-                if (in_word) in_word = false;
-            } else {
-                in_word = true;
-                words += 1;
-            }
-        }
-    }
-
-    switch (options) {
-        'c' => std.debug.print("{d} \n", .{bytes}),
-        'l' => std.debug.print("{d} \n", .{lines}),
-        'w' => std.debug.print("{d} \n", .{words}),
-        'm' => std.debug.print("{d} \n", .{chars}),
-        else => {
-            if (file_name) |f_name| {
-                std.debug.print("{d} {d} {d} {s}\n", .{ lines, words, bytes, f_name });
-            } else {
-                std.debug.print("{d} {d} {d}\n", .{ lines, words, bytes });
-            }
-        },
     }
 }
