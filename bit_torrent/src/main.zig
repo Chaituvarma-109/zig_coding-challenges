07827aad163557c6c491a1f97f4c1685d6b9f7d4
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2025-12-07 00:55:57 +0530

    reading torrent files.

--- bit_torrent/src/main.zig
+++ bit_torrent/src/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
+const dcode = @import("bencode/decode.zig");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const alloc = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(alloc);
+    defer alloc.free(args);
+
+    const torrent_file = args[1];
+
+    const f = try std.fs.openFileAbsolute(torrent_file, .{});
+    defer f.close();
+
+    const data = try f.readToEndAlloc(alloc, std.fs.max_path_bytes);
+
+    dcode.decode(alloc, data);
 }
