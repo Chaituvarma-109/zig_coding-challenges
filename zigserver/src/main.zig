749d3484ef628eb2d64f688bb98d65a677f67588
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2026-01-07 11:58:38 +0530

    updated to zig version 0.16.

--- zigserver/src/main.zig
+++ zigserver/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
-const net = std.net;
-const Pool = std.Thread.Pool;
+const Allocator = std.mem.Allocator;
+const Io = std.Io;
+const net = Io.net;
 
 const Request = struct {
     method: []const u8,
@@ -8,10 +9,10 @@ const Request = struct {
 
     fn new(buff: []u8) Request {
         var splitter = std.mem.splitSequence(u8, buff, "\r\n");
-        const rqst = splitter.next().?;
+        const rqst: []const u8 = splitter.next().?;
         var trim_path = std.mem.splitSequence(u8, rqst, " ");
-        const rqst_method = trim_path.next().?;
-        const rqst_path = trim_path.next().?;
+        const rqst_method: []const u8 = trim_path.next().?;
+        const rqst_path: []const u8 = trim_path.next().?;
 
         return Request{
             .method = rqst_method,
@@ -21,67 +22,83 @@ const Request = struct {
 };
 
 pub fn main() !void {
-    const page_alloc = std.heap.page_allocator;
+    const page_alloc: Allocator = std.heap.page_allocator;
 
-    var pool: std.Thread.Pool = undefined;
-    try pool.init(Pool.Options{ .n_jobs = 4, .allocator = page_alloc });
-    defer pool.deinit();
+    var io_threaded: Io.Threaded = .init(page_alloc, .{ .concurrent_limit = .limited(4) });
+    defer io_threaded.deinit();
 
-    const address = try net.Address.resolveIp("127.0.0.1", 8080);
-    var listener = try address.listen(.{
+    var io: Io = io_threaded.io();
+
+    const address: net.IpAddress = try .parse("127.0.0.1", 8080);
+    var listener: net.Server = try address.listen(io, .{
         .reuse_address = true,
     });
-    defer listener.deinit();
+    defer listener.deinit(io);
 
     while (true) {
-        const conn = listener.accept() catch |err| {
+        const conn: net.Stream = listener.accept(io) catch |err| {
             std.debug.print("Error accepting connection: {any}\n", .{err});
             continue;
         };
 
-        try pool.spawn(handleRequest, .{conn});
+        var fut = try io.concurrent(handleRequest, .{ page_alloc, conn, io });
+        _ = fut.await(io);
     }
 }
 
-fn handleRequest(conn: net.Server.Connection) void {
-    defer conn.stream.close();
+fn handleRequest(alloc: Allocator, conn: net.Stream, io: Io) void {
+    defer conn.close(io);
 
-    var r = conn.stream.reader(&.{});
-    var w = conn.stream.writer(&.{});
+    var r: Io.net.Stream.Reader = conn.reader(io, &.{});
+    var w: Io.net.Stream.Writer = conn.writer(io, &.{});
 
-    var buff: [1024]u8 = undefined;
+    const reader: *Io.Reader = &r.interface;
+    const writer: *Io.Writer = &w.interface;
 
-    const n = r.file_reader.readStreaming(&buff) catch |err| {
-        std.debug.print("Error reading from connection: {any}\n", .{err});
+    var resp_wr: Io.Writer.Allocating = .init(alloc);
+    defer resp_wr.deinit();
+
+    _ = reader.stream(&resp_wr.writer, .unlimited) catch |err| {
+        std.log.err("err: {any}\n", .{err});
         return;
     };
 
-    const req = Request.new(buff[0..n]);
-
+    const buff: []u8 = resp_wr.written();
+    const req: Request = .new(buff);
     var r_buff: [1024]u8 = undefined;
 
     if (std.mem.eql(u8, req.path, "/") or std.mem.eql(u8, req.path, "/index.html")) {
-        const http_resp = "HTTP/1.1 200 OK";
+        const http_resp: []const u8 = "HTTP/1.1 200 OK";
         var f_buff: [1024]u8 = undefined;
-        const contents = std.fs.cwd().readFile("./www/index.html", &f_buff) catch |err| {
-            std.debug.print("Error reading file: {any}\n", .{err});
+        const contents: []u8 = Io.Dir.readFile(.cwd(), io, "./www/index.html", &f_buff) catch |err| {
+            std.log.err("Error reading file: {any}\n", .{err});
             return;
         };
-        const resp = std.fmt.bufPrint(&r_buff, "{s}\r\n\r\n{s}", .{ http_resp, contents }) catch |err| {
-            std.debug.print("Error formatting response: {any}\n", .{err});
+        const resp: []u8 = std.fmt.bufPrint(&r_buff, "{s}\r\n\r\n{s}", .{ http_resp, contents }) catch |err| {
+            std.log.err("Error formatting response: {any}\n", .{err});
             return;
         };
-        w.interface.writeAll(resp) catch |err| {
-            std.debug.print("Error writing response: {any}\n", .{err});
+        writer.writeAll(resp) catch |err| {
+            std.log.err("err: {any}\n", .{err});
+            return;
+        };
+        writer.flush() catch |err| {
+            std.log.err("err: {any}\n", .{err});
+            return;
         };
     } else {
-        const http_resp = "HTTP/1.1 404 Not Found";
-        const resp = std.fmt.bufPrint(&r_buff, "{s}\r\n\r\n", .{http_resp}) catch |err| {
-            std.debug.print("Error formatting response: {any}\n", .{err});
+        const http_resp: []const u8 = "HTTP/1.1 404 Not Found";
+        const resp: []u8 = std.fmt.bufPrint(&r_buff, "{s}\r\n\r\n", .{http_resp}) catch |err| {
+            std.log.err("Error formatting response: {any}\n", .{err});
             return;
         };
-        w.interface.writeAll(resp) catch |err| {
-            std.debug.print("Error writing response: {any}\n", .{err});
+        writer.writeAll(resp) catch |err| {
+            std.log.err("Error writing response: {any}\n", .{err});
+            return;
+        };
+        writer.flush() catch |err| {
+            std.log.err("err: {any}\n", .{err});
+            return;
         };
     }
 }
