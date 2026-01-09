85cf5d0aa7bb3c9c40f7c8f5ecea8f20fb632e50
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2026-01-09 13:56:08 +0530

    updated to zig version 0.16.x.

--- zzd/src/main.zig
+++ zzd/src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
-const File = std.fs.File;
-const Writer = std.Io.Writer;
-const Reader = std.Io.Reader;
+
+const Io = std.Io;
+const mem = std.mem;
+const process = std.process;
+const File = Io.File;
+const Writer = Io.Writer;
+const Reader = Io.Reader;
 
 const Config = struct {
     file: []const u8 = undefined,
@@ -12,33 +16,30 @@ const Config = struct {
     seek: usize = 0,
     revert: bool = false,
 
-    fn init(args: [][:0]u8) !Config {
-        var i: usize = 0;
+    fn init(args: process.Args, alloc: mem.Allocator) !Config {
+        var arg_iter = try args.iterateAllocator(alloc);
+        defer arg_iter.deinit();
+
+        _ = arg_iter.skip();
 
         var cliargs = Config{};
 
-        while (i < args.len) : (i += 1) {
-            const arg = args[i];
+        while (arg_iter.next()) |arg| {
             if (std.mem.eql(u8, arg, "-e")) {
                 cliargs.endian = true;
                 cliargs.chunklen = 4;
             } else if (std.mem.eql(u8, arg, "-g")) {
-                i += 1;
-                cliargs.chunklen = try std.fmt.parseInt(usize, args[i], 10);
+                cliargs.chunklen = try std.fmt.parseInt(usize, arg_iter.next() orelse return error.ChunklenArgMissing, 10);
             } else if (std.mem.eql(u8, arg, "-l")) {
-                i += 1;
-                cliargs.linelen = try std.fmt.parseInt(usize, args[i], 10);
+                cliargs.linelen = try std.fmt.parseInt(usize, arg_iter.next() orelse return error.LinelenArgMissing, 10);
             } else if (std.mem.eql(u8, arg, "-c")) {
-                i += 1;
-                cliargs.columns = try std.fmt.parseInt(usize, args[i], 10);
+                cliargs.columns = try std.fmt.parseInt(usize, arg_iter.next() orelse return error.ColumnArgMissing, 10);
             } else if (std.mem.eql(u8, arg, "-s")) {
-                i += 1;
-                cliargs.seek = try std.fmt.parseInt(usize, args[i], 10);
+                cliargs.seek = try std.fmt.parseInt(usize, arg_iter.next() orelse return error.SeekArgMissing, 10);
             } else if (std.mem.eql(u8, arg, "-r")) {
                 cliargs.revert = true;
             } else if (std.mem.eql(u8, arg, "-f")) {
-                i += 1;
-                cliargs.file = args[i];
+                cliargs.file = arg_iter.next() orelse return error.fileArgumentMissing;
             }
         }
 
@@ -46,42 +47,48 @@ const Config = struct {
     }
 };
 
-pub fn main() !void {
-    const pga: std.mem.Allocator = std.heap.page_allocator;
-    const args = try std.process.argsAlloc(pga);
-    defer std.process.argsFree(pga, args);
+pub fn main(init: process.Init.Minimal) !void {
+    const pga: mem.Allocator = std.heap.page_allocator;
 
-    const arg: Config = try Config.init(args);
+    const args: process.Args = init.args;
+    const arg: Config = try Config.init(args, pga);
 
-    const file: File = try std.fs.cwd().openFile(arg.file, .{});
-    defer file.close();
+    var io_threaded: Io.Threaded = .init_single_threaded;
+    const io: Io = io_threaded.io();
 
-    const s: File.Stat = try file.stat();
-    const file_size: u64 = s.size;
+    const f: File = try Io.Dir.openFile(.cwd(), io, arg.file, .{ .mode = .read_write });
+    defer f.close(io);
 
-    var wbuff: [500]u8 = undefined;
-    var stdout: File = std.fs.File.stdout();
-    defer stdout.close();
-    var fw: File.Writer = stdout.writer(&wbuff);
+    const stat: File.Stat = try f.stat(io);
+    const size: u64 = stat.size;
 
-    var buff: [500]u8 = undefined;
-    var fr: File.Reader = file.reader(&buff);
+    const rbuff: []u8 = try pga.alloc(u8, size);
+    defer pga.free(rbuff);
+    const buf: []u8 = try Io.Dir.readFile(.cwd(), io, arg.file, rbuff);
+
+    var wbuff: [1024]u8 = undefined;
+    var stdout: File = .stdout();
+    defer stdout.close(io);
+    var fw: File.Writer = stdout.writer(io, &wbuff);
+    const fwr = &fw.interface;
+
+    var buff: [1024]u8 = undefined;
+    var fr: File.Reader = f.reader(io, &buff);
 
     if (arg.revert) {
-        try revertdump(&fr, &fw.interface);
+        try revertdump(&fr.interface, &fw.interface);
     } else {
-        try dumpHex(&fr, &fw.interface, pga, file_size, arg, .escape_codes);
+        try dumpHex(buf, arg, .{ .writer = fwr, .mode = .escape_codes });
     }
 }
 
-fn dumpHex(rd: *File.Reader, bw: *Writer, pga: std.mem.Allocator, file_size: u64, args: Config, cfg: std.Io.tty.Config) !void {
+fn dumpHex(buff: []u8, args: Config, tty: std.Io.Terminal) !void {
+    const bw = tty.writer;
+
     const chunklen: usize = args.chunklen;
     const columns: usize = args.columns;
 
-    var buff: []u8 = try pga.alloc(u8, file_size);
-    defer pga.free(buff);
-    const n: usize = try rd.read(buff);
-    const limit: usize = @min(args.linelen orelse n, n);
+    const limit: usize = @min(args.linelen orelse buff.len, buff.len);
     const start: usize = args.seek;
     const end: usize = @max(limit, limit + args.seek);
 
@@ -90,24 +97,28 @@ fn dumpHex(rd: *File.Reader, bw: *Writer, pga: std.mem.Allocator, file_size: u64
 
     while (chunks.next()) |window| {
         // 1. Print the address.
-        try cfg.setColor(bw, .dim);
+        try tty.setColor(.dim);
         try bw.print("{x:0>8}  ", .{line_offset * columns + args.seek});
-        try cfg.setColor(bw, .reset);
+        try tty.setColor(.reset);
+        try bw.flush();
 
         // 2. Print the bytes.
-        var lit = std.mem.window(u8, window, chunklen, chunklen);
+        var lit = mem.window(u8, window, chunklen, chunklen);
         while (lit.next()) |chunk| {
-            try cfg.setColor(bw, .green);
+            try tty.setColor(.green);
             if (args.endian) {
-                var iter = std.mem.reverseIterator(chunk);
+                var iter = mem.reverseIterator(chunk);
                 while (iter.next()) |byte| {
                     try bw.print("{x:0>2}", .{byte});
+                    try bw.flush();
                 }
             } else {
                 try bw.printHex(chunk, .lower);
+                try bw.flush();
             }
-            try cfg.setColor(bw, .reset);
+            try tty.setColor(.reset);
             try bw.writeByte(' ');
+            try bw.flush();
         }
 
         // print spaces
@@ -117,45 +128,46 @@ fn dumpHex(rd: *File.Reader, bw: *Writer, pga: std.mem.Allocator, file_size: u64
 
             for (0..missing_bytes) |_| {
                 try bw.print("  ", .{});
+                try bw.flush();
             }
             for (0..missing_chunks) |_| {
                 try bw.writeByte(' ');
+                try bw.flush();
             }
         }
         try bw.writeByte(' ');
+        try bw.flush();
 
         // 3. Print the characters.
         for (window) |byte| {
             if (std.ascii.isPrint(byte)) {
-                try cfg.setColor(bw, .green);
+                try tty.setColor(.green);
                 try bw.writeByte(byte);
-                try cfg.setColor(bw, .reset);
+                try tty.setColor(.reset);
+                try bw.flush();
             } else {
                 try bw.writeByte('.');
+                try bw.flush();
             }
         }
         try bw.writeByte('\n');
+        try bw.flush();
         line_offset += 1;
     }
-    try bw.flush();
 }
 
-fn revertdump(rd: *File.Reader, wr: *Writer) !void {
-    while (rd.interface.takeDelimiterExclusive('\n')) |line| {
+fn revertdump(rd: *Io.Reader, wr: *Writer) !void {
+    while (true) {
+        const ln: []u8 = try rd.takeDelimiter('\n') orelse return;
         // Split by the offset mark
-        var parts = std.mem.tokenizeAny(u8, line, ":");
+        var parts = mem.tokenizeAny(u8, ln, ":");
         _ = parts.next(); // Remove offset
-        var group = std.mem.tokenizeSequence(u8, parts.next() orelse continue, "  ");
-        var hexes = std.mem.tokenizeAny(u8, group.next() orelse continue, " ");
+        var group = mem.tokenizeSequence(u8, parts.next() orelse continue, "  ");
+        var hexes = mem.tokenizeAny(u8, group.next() orelse continue, " ");
         var out: [50]u8 = undefined;
         while (hexes.next()) |hexstr| {
             try wr.print("{s}", .{try std.fmt.hexToBytes(&out, hexstr)});
+            try wr.flush();
         }
-    } else |err| switch (err) {
-        error.EndOfStream => try wr.flush(),
-        else => {
-            std.debug.print("err: {}\n", .{err});
-            return;
-        },
     }
 }
