fa4ed6d05345245956c23370d8338da75be9fa5e
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2026-02-13 17:53:53 +0530

    implemented sweep scan without cidr notation.

--- portscanner/src/main.zig
+++ portscanner/src/main.zig
@@ -4,13 +4,11 @@ const mem = std.mem;
 const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
-    const arena: std.mem.Allocator = init.arena.allocator();
-    _ = arena;
     const io = init.io;
 
-    // var stdout_buffer: [1024]u8 = undefined;
-    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    // const stdout_writer = &stdout_file_writer.interface;
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    const wr = &stdout_file_writer.interface;
 
     var host: []const u8 = undefined;
     var port_str: ?[]const u8 = null;
@@ -28,38 +26,62 @@ pub fn main(init: std.process.Init) !void {
         }
     }
 
-    if (port_str) |p| {
-        const port = try std.fmt.parseInt(u16, p, 10);
-
-        const address: Io.net.IpAddress = try .parse(host, port);
-        var listener: Io.net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch |err| {
-            std.log.err("err: {}\n", .{err});
-            std.log.info("port: {d} is close\n", .{port});
-            return;
-        };
-        defer listener.close(io);
-
-        std.log.info("port: {d} is open\n", .{port});
-    } else {
+    if (std.mem.containsAtLeast(u8, host, 1, ",")) {
         var group: Io.Group = .init;
 
-        try group.concurrent(io, vanillaScan, .{ io, host });
+        try group.concurrent(io, sweepScan, .{ io, host, wr });
         try group.await(io);
+    } else {
+        if (port_str) |p| {
+            const port = try std.fmt.parseInt(u16, p, 10);
 
-        // var f = try io.concurrent(vanillaScan, .{ io, host });
-        // try f.await(io);
+            const address: Io.net.IpAddress = try .parse(host, port);
+            var listener: Io.net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch |err| {
+                std.log.err("err: {}\n", .{err});
+                std.log.info("port: {d} is close\n", .{port});
+                return;
+            };
+            defer listener.close(io);
+
+            try wr.print("port: {d} is open\n", .{port});
+            try wr.flush();
+        } else {
+            var group: Io.Group = .init;
+
+            try group.concurrent(io, vanillaScan, .{ io, host, wr });
+            try group.await(io);
+        }
     }
 }
 
-fn vanillaScan(io: Io, host: []const u8) !void {
-    for (1..65536) |i| {
-        const port: u16 = @intCast(i);
-        const address = Io.net.IpAddress.parse(host, port) catch return;
+fn vanillaScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
+    for (1..65536) |port| {
+        const address = Io.net.IpAddress.parse(host, @intCast(port)) catch continue;
 
-        // const timeout_ns: i128 = 2000 * std.time.ns_per_ms;
+        // const timeout_ns: i96 = 2000 * std.time.ns_per_ms;
+        // const duration: Io.Clock.Duration = .{ .clock = .cpu_thread, .raw = .{ .nanoseconds = timeout_ns } };
+
+        // const dd = Io.Clock.Timestamp.fromNow(io, duration);
+        // const t: Io.Timeout = .{ .deadline = .fromNow(io, duration) };
+
         var listener: Io.net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch continue;
+        defer listener.close(io);
 
-        std.log.info("port {d} is open", .{port});
-        listener.close(io);
+        wr.print("host {s} port {d} is open\n", .{ host, port }) catch |err| {
+            std.log.err("{any}\n", .{err});
+        };
+        wr.flush() catch |err| {
+            std.log.err("{any}\n", .{err});
+        };
     }
+
+    return;
+}
+
+fn sweepScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
+    var host_iter = std.mem.splitSequence(u8, host, ",");
+    while (host_iter.next()) |h| {
+        vanillaScan(io, h, wr) catch continue;
+    }
+    return;
 }
