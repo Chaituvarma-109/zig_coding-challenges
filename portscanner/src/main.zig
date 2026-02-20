23c043fba7e499a1ec311a3f7395fb7440cdb2c4
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2026-02-20 16:45:34 +0530

    refacor: changed from concurrent to async.

--- portscanner/src/main.zig
+++ portscanner/src/main.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 
 const mem = std.mem;
 const Io = std.Io;
+const net = Io.net;
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
+    const io: Io = init.io;
 
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const wr = &stdout_file_writer.interface;
+    const wr: *Io.Writer = &stdout_file_writer.interface;
 
     var host: []const u8 = undefined;
     var port_str: ?[]const u8 = null;
@@ -19,44 +20,42 @@ pub fn main(init: std.process.Init) !void {
     _ = args.skip();
 
     while (args.next()) |arg| {
-        if (mem.eql(u8, arg, "-host")) {
+        if (mem.eql(u8, arg, "host")) {
             host = args.next().?;
-        } else if (mem.eql(u8, arg, "-port")) {
+        } else if (mem.eql(u8, arg, "port")) {
             port_str = args.next();
         }
     }
 
-    if (std.mem.containsAtLeast(u8, host, 1, ",")) {
+    if (port_str) |p| {
+        const port = try std.fmt.parseInt(u16, p, 10);
+
+        const address: net.IpAddress = try .parse(host, port);
+        var listener: net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch |err| {
+            std.log.err("err: {}\n", .{err});
+            std.log.info("port: {d} is close\n", .{port});
+            return;
+        };
+        defer listener.close(io);
+
+        try wr.print("port: {d} is open\n", .{port});
+        try wr.flush();
+    } else {
         var group: Io.Group = .init;
 
-        try group.concurrent(io, sweepScan, .{ io, host, wr });
-        try group.await(io);
-    } else {
-        if (port_str) |p| {
-            const port = try std.fmt.parseInt(u16, p, 10);
-
-            const address: Io.net.IpAddress = try .parse(host, port);
-            var listener: Io.net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch |err| {
-                std.log.err("err: {}\n", .{err});
-                std.log.info("port: {d} is close\n", .{port});
-                return;
-            };
-            defer listener.close(io);
-
-            try wr.print("port: {d} is open\n", .{port});
-            try wr.flush();
+        if (std.mem.containsAtLeast(u8, host, 1, ",")) {
+            group.async(io, sweepScan, .{ io, host, wr });
         } else {
-            var group: Io.Group = .init;
-
-            try group.concurrent(io, vanillaScan, .{ io, host, wr });
-            try group.await(io);
+            group.async(io, vanillaScan, .{ io, host, wr });
         }
+
+        try group.await(io);
     }
 }
 
 fn vanillaScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
     for (1..65536) |port| {
-        const address = Io.net.IpAddress.parse(host, @intCast(port)) catch continue;
+        const address = net.IpAddress.parse(host, @intCast(port)) catch continue;
 
         // const timeout_ns: i96 = 2000 * std.time.ns_per_ms;
         // const duration: Io.Clock.Duration = .{ .clock = .cpu_thread, .raw = .{ .nanoseconds = timeout_ns } };
@@ -64,7 +63,7 @@ fn vanillaScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
         // const dd = Io.Clock.Timestamp.fromNow(io, duration);
         // const t: Io.Timeout = .{ .deadline = .fromNow(io, duration) };
 
-        var listener: Io.net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch continue;
+        var listener: net.Stream = address.connect(io, .{ .mode = .stream, .protocol = .tcp, .timeout = .none }) catch continue;
         defer listener.close(io);
 
         wr.print("host {s} port {d} is open\n", .{ host, port }) catch |err| {
@@ -79,7 +78,7 @@ fn vanillaScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
 }
 
 fn sweepScan(io: Io, host: []const u8, wr: *Io.Writer) !void {
-    var host_iter = std.mem.splitSequence(u8, host, ",");
+    var host_iter = mem.splitSequence(u8, host, ",");
     while (host_iter.next()) |h| {
         vanillaScan(io, h, wr) catch continue;
     }
