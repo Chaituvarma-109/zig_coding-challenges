1b2871c78cc42680ebf4ec7b265d923c994a1c73
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2026-05-03 08:58:14 +0530

    added lexopts cli library to parse cli args.

--- qrcodegenerator/build.zig
+++ qrcodegenerator/build.zig
@@ -4,14 +4,31 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const translate_c = b.addTranslateC(.{
+        .root_source_file = b.path("src/c.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const c_mod = translate_c.createModule();
+
     const exe = b.addExecutable(.{
         .name = "qrcodegenerator",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{
+                    .name = "c",
+                    .module = c_mod,
+                },
+            },
         }),
     });
+
+    const lexopts = b.dependency("lexopts", .{});
+    exe.root_module.addImport("lexopts", lexopts.module("lexopts"));
 
     b.installArtifact(exe);
 
@@ -33,4 +50,64 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
+
+    const mode_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/modes.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(mode_tests).step);
+
+    const encode_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/encode.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "c", .module = c_mod },
+            },
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(encode_tests).step);
+
+    const kanji_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/modeEncode/kanji.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "c", .module = c_mod },
+            },
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(kanji_tests).step);
+
+    const numeric_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/modeEncode/numerical.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(numeric_tests).step);
+
+    const alphanumeric_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/modeEncode/alphanumerical.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(alphanumeric_tests).step);
+
+    const byte_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/modeEncode/byte.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    test_step.dependOn(&b.addRunArtifact(byte_tests).step);
 }
